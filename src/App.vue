1f9bad4c78f0dcb51e4fdad8c62c897fe405dae3
lsylk <leslye.lk@gmail.com>
2019-11-23 23:49:13 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <DrawerNavbar />
+  </div>
+</template>
+
+<script>
+
+import DrawerNavbar from "./components/DrawerNavbar";
+
+export default {
+  name: 'app',
+  components: {
+    DrawerNavbar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
