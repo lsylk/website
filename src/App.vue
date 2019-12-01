074e073919439014bd2ed6026193fce779c30bbb
lsylk <leslye.lk@gmail.com>
2019-12-01 17:57:23 +0100

    fix: footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <DrawerNavbar />
+  <div>
+    <div id="app">
+      <DrawerNavbar />
+    </div>
     <AppFooter />
   </div>
 </template>
@@ -25,5 +27,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+@media (min-height: 723px) {
+  #app {
+    min-height: calc(100vw - 667px);
+    background-color: white;
+  }
+}
+
+@media (max-width: 1580px) {
+  #app {
+    min-height: calc(100vh - 30px);
+    background-color: white;
+  }
 }
 </style>
